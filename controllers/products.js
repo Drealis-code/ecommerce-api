b7e80e3975b4fa7fd8fe5418eaf4b6a104acea8a
Lisa <lisa.baiden26@gmail.com>
2025-03-07 16:50:03 +0000

    seoond commit

--- controllers/products.js
+++ controllers/products.js
@@ -1,19 +1,38 @@
-export const addProduct = (req, res) =>{
-    res.send('Product added!');
-}
+import { ProductModel } from "../models/product.js";
 
-export const getProducts = (req, res) => {
-    res.send('All products!');
-}
+export const addProduct = async (req, res, next) => {
+ try {
+   // upload product image
+   // validate product information
+   // save product information in database
+   const result = await ProductModel.create(req.body);
+   // return response
+   res.json(result);
+ } catch (error) {
+  next(error);
+ }
+};
+
+export const getProducts = async (req, res) => {
+ try {
+   // fetch products from database
+   const result = await ProductModel.find();
+   // return response
+   res.json(result);
+ } catch (error) {
+  next(error);
+  
+ }
+};
 
 export const countProducts = (req, res) => {
-    res.send('All products count!');
-}
+  res.send("All products count!");
+};
 
 export const updateProduct = (req, res) => {
-    res.send(`Product with id ${req.params.id} updated!`);
-}
+  res.send(`Product with id ${req.params.id} updated!`);
+};
 
 export const deleteProduct = (req, res) => {
-    res.send(`Product with id ${req.params.id} deleted!`);
-}
+  res.send(`Product with id ${req.params.id} deleted!`);
+};
--- index.js
+++ index.js
@@ -1,8 +1,16 @@
 import express from 'express';
 import productsRouter from './routes/products.js';
+import mongoose from 'mongoose';
+
+// make database connection
+ 
+await mongoose.connect(process.env.MONGO_URL);
 
 // create an express app
 const app = express();
+
+// use global middlewares
+app.use(express.json());
 
 // use routes
 app.use(productsRouter);
